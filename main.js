ef166f280cbadc1c23f420b1366b7c7d7a6d75f5
PCWitcher <pcwitcher.ie@gmail.com>
2019-06-18 12:56:56 +0100

    feat: add js string

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const autor = 'Zbigniew Drelich';
 
 console.log(`W końcu znalazłaś/eś się w odpowiednim miejscu. ${autor} wita serdecznie.`);
+
+const paragraph = document.querySelector(`.paragraph-js`);
+paragraph.innerHTML = `Nazywam się ${autor} i jestem odpowiedzialny za ten bałagan.`;
